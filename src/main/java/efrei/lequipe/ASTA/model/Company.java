a8090d6c886800af01bac66b6dcb221c80ecefe4
Selim Haddioui <selim.haddioui@hotmail.com>
2023-10-12 03:54:15 +0200

    Removing complex class, framework items like custom response object and class not used.

--- src/main/java/efrei/lequipe/ASTA/model/Company.java
+++
@@ -1,4 +0,0 @@
-package efrei.lequipe.ASTA.model;
-
-public record Company(String id, String name, String address, String usefulInformation) {
-}
--- src/main/java/efrei/lequipe/ASTA/model/Tutor.java
+++
@@ -1,6 +0,0 @@
-package efrei.lequipe.ASTA.model;
-
-public record Tutor(String id, String lastName, String firstName, String jobName, String email, String phoneNumber,
-                    String remarks) {
-
-}
